9f9bda877be98d4d40198b1b14ca9790c53b6fc2
Stanley <offorstanley4real@gmail.com>
2025-01-18 00:17:39 +0100

    solved reverse string

---
+++ reverseString.js
@@ -0,0 +1,30 @@
+// 344. Reverse String
+// Easy
+// Topics
+// Companies
+// Hint
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+
+var reverseString = function(s) {
+    return s.reverse() 
+};
+
+// console.log(reverseString(["h","e","l","l","o"])); 
